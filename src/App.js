ffaafb304f19e5fcb069ba5f933ad83e62eee215
FedericoBuccella <fedec.fb@gmail.com>
2022-03-21 09:58:00 -0300

    Ordenado de titulo en un div correspondiente

--- src/App.js
+++ src/App.js
@@ -7,11 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
-      <div>
         <h1>
           hola bienvenidos a react js
         </h1>
-      </div>
     </div>
   );
 }
