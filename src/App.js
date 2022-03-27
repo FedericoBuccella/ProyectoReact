339e921435dcc8345427dd4f565489e1bb08c351
FedericoBuccella <fedec.fb@gmail.com>
2022-03-27 15:28:09 -0300

    Creacion navBar y logo principal

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/Navbar/NavBar';
+import logo from './LogoTC.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App' >
+      <header>
+        <img src={logo} className="logoPrinci"></img>
+        <NavBar />
       </header>
-        <h1>
-          hola bienvenidos a react js
-        </h1>
     </div>
   );
 }
