9b269b0b2ef0ec89b40008eed0ddadfddd25aa23
FedericoBuccella <fedec.fb@gmail.com>
2022-04-09 09:43:50 -0300

    asd

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,21 +1,12 @@
 import { Card } from "react-bootstrap";
 
-const Item = ({name, id, price, category, img}) => {
+const Item = ({name, img}) => {
     return (
         <section>
             <Card style={{ width: '18rem' }}>
                 <Card.Img variant="top" src={img} />
                 <Card.Body>
                     <Card.Title>{name}</Card.Title>
-                    <Card.Text>
-                       {id}
-                    </Card.Text>
-                    <Card.Text>
-                       {category}
-                    </Card.Text>
-                    <Card.Text>
-                       {price}
-                    </Card.Text>
                     <div>
                         <button variant="primary">Ver Detalles</button>
                     </div>
