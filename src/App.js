476eda2b79fd144403997f3e7f042786a07b630a
FedericoBuccella <fedec.fb@gmail.com>
2022-04-08 23:48:33 -0300

    Details

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import NavBar from './components/Navbar/NavBar';
-import ItemListContainer from './components/ItemListContainer/itemListContainer';
 import ItemCount from './components/ItemCount/ItemCount';
 
 function App() {
@@ -9,7 +8,6 @@ function App() {
     <div className='App' >
       <header>
         <NavBar />
-        <ItemListContainer greeting='Bienvenidos a TecnoCenter' />
         <ItemCount initial={1} stock={10} />
       </header>
     </div>
---
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ItemDetails from "../ItemDetails/ItemDetails";
+
+
+const ItemDetailContainer = () => {
+    return (
+        <div>
+            <ItemDetails />
+        </div>
+    )
+}
+
+export default ItemDetailContainer
